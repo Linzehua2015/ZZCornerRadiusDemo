893923122523f408ba10cce3270d80323d6d73d5
ZahiLin <332960910@qq.com>
2018-07-15 10:14:23 +0800

    重新提

--- ZZCornerRadiusDemo/ViewController.m
+++ ZZCornerRadiusDemo/ViewController.m
@@ -7,12 +7,25 @@
 //
 
 #import "ViewController.h"
-
+#import "UIView-CornerRadius.h"
 
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"test"]];
+    imageView.backgroundColor = [UIColor yellowColor];
+//    imageView.contentMode = UIViewContentModeScaleAspectFill;
+    imageView.frame = CGRectMake(100, 100, 100, 100);
+//    imageView.layer.masksToBounds = YES;
+    [self.view addSubview:imageView];
+    
+    [imageView addImageWithCornerRadius:50 color:[UIColor redColor]];
+}
 
 @end
